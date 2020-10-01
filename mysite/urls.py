462940cd8e55036f2cc8e14f79c28e7fd1f3b791
Nishant Kumar Singh <52786028+nishu9162306344@users.noreply.github.com>
2020-10-01 21:43:30 +0530

    Update urls.py

--- mysite/urls.py
+++ mysite/urls.py
@@ -13,9 +13,13 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+#This all url are used To rendering the path 
+#this is base where all routes are open
+
 from django.contrib import admin
 from django.urls import path
 from . import views
+
 
 
 urlpatterns = [
